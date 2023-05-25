9195d2da990dfe18dec2c470db940025612bb4d2
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-05-25 16:25:05 +0200

    printed days in page with call countdown function on a timer of 1 second

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ console.log(currentDate);
 let lessonDate = new Date("May 26, 2023 09:30:00");
 
 console.log(lessonDate);
+
+let remainingTime = lessonDate - currentDate;
+
+let countDown = setInterval(callCaountDown, 1000);
+
+function callCaountDown(){
+    let daysCountDown = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+
+    console.log(daysCountDown);
+
+    document.getElementById("daysTimer").innerHTML = daysCountDown;
+}
+
